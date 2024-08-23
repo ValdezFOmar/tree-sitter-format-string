710d359389b60615b9c1b9625bb8c34e20ad5ff8
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-08-23 03:56:09 -0700

    feat(queries): add queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,1 +1,33 @@
-; NOTE: add highlight queries here
+(identifier) @variable
+
+(attribute_expression
+  attribute: (identifier) @variable.member)
+
+(integer) @number
+
+(item_string) @string
+
+(conversion) @function.macro
+
+; Highlights for format spec...
+; (format)
+
+[
+  "."
+  ":"
+] @punctuation.delimiter
+
+[
+  "["
+  "]"
+] @punctuation.bracket
+
+[
+  "{"
+  "}"
+] @punctuation.special
+
+[
+  "{{"
+  "}}"
+] @string.escape
